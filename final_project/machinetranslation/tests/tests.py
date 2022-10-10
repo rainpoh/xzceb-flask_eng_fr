3c95b019201577406335e7602bc846374868a486
rainpoh <hweething@gmail.com>
2022-10-10 00:14:55 -0400

    Final changes

---
+++ final_project/machinetranslation/tests/tests.py
@@ -0,0 +1,13 @@
+import unittest
+
+from translator import english_to_french, french_to_english
+
+class test_english_to_french(unittest.TestCase):
+    def test1(self):
+        self.assertIsNotNone(english_to_french(0))
+        self.assertEqual(english_to_french('Hello'), 'Bonjour')
+    def test2(self):
+        self.assertIsNotNone(french_to_english(0))
+        self.assertEqual(french_to_english('Bonjour'), 'Hello')
+     
+unittest.main()
--- final_project/server.py
+++ final_project/server.py
@@ -7,18 +7,25 @@ app = Flask("Web Translator")
 @app.route("/englishToFrench")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to French"
+    translation = language_translator.translate(
+    text=textToTranslate,
+    model_id='en-fr').get_result()
+    french_text = translation['translations'][0].get('translation')
+    return french_text
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to English"
+    translation = language_translator.translate(
+    text=textToTranslate,
+    model_id='fr-en').get_result()
+    english_text = translation['translations'][0].get('translation')
+    return english_text
 
 @app.route("/")
 def renderIndexPage():
     # Write the code to render template
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
